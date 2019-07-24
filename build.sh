cdb01790d5ff66f3f35405f241012fa4f33f3af4
atm959 <Aaron.Murphyaaron@gmail.com>
2019-07-23 22:02:19 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-
+(This build script is non-functioning)
 
 nasm -f elf32 main.asm -o main.o
-$TARGET-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
-$TARGET-gcc -T linker.ld -o myos.bin -ffreestanding -O2 -nostdlib main.o kernel.o -lgcc
+i686-elf-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+i686-elf-gcc -T linker.ld -o myos.bin -ffreestanding -O2 -nostdlib main.o kernel.o -lgcc
 dd status=noxfer conv=notrunc if=myos.bin of=osthing.img
